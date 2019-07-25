1cd8960436d242ebf56a7214d47f9fa3888b7055
wangyonghao <wangyonghao@rongcloud.cn>
2019-07-25 12:40:24 +0800

    feat: sealmeeting web

--- web/config.js
+++ web/config.js
@@ -1,8 +1,8 @@
 var global_config = {
-  server_path: 'https://server_path',
-  file_server_path: 'https://file_server_path',
+  server_path: 'https://sealmeeting.rongcloud.cn',
+  file_server_path: 'https://sealclass.rongcloud.cn',
   load_interval : 1 * 1000,
   width : 2800,
   height : 2100,
-  seal_class_server_path: 'https://sealclass_server_path'
+  seal_class_server_path: 'https://api-sealmeeting.rongcloud.cn/sealmeeting/api/v1'
 };
